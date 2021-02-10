27ba4326b6f6c38ccdfd8dee52350688e193ee92
Yanina92 <78880661+Yanina92@users.noreply.github.com>
2021-02-10 18:19:37 -0300

    Update TpIntermedio.py

--- TpIntermedio.py
+++ TpIntermedio.py
@@ -7,14 +7,6 @@ def iniciar_sesion():
     ventana2.mainloop()
 
 
-ventana1 = Tk()
-ventana1.geometry("500x200")
-ventana1.resizable(0, False)
-# ventana1.config()
-fondo1 = photoImage(file="Fondo1.gif")
-etiqueta_fondo1 = Label(ventana1, image=fondo1).place(x=0, y=0)
-Label(ventana1, text="Usted ya esta registrado?").grid(
-    row=1, column=0, sticky=E, padx=175
 )
 boton = Button(ventana1, text="Iniciar sesion", command=iniciar_sesion).grid(
     row=2, column=0
